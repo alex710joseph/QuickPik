93bac42745023ab9d4818e52af12e083caf2b273
alex710joseph <alexjoseph710@gmail.com>
2026-03-18 23:50:50 -0400

    setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import IndexPage from './components'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <IndexPage />
   </StrictMode>,
 )
